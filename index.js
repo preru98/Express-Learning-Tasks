308a0dd5f17cb212bd633662d7cb9aa5574c5c16
Prerna <prerna1998.ps@gmail.com>
2020-07-12 07:30:11 +0530

    Morgan | Serve Static Files

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const http = require('http')
+const morgan =  require('morgan')
 
 const port = 3000
 const hostname = 'localhost' 
 
 const app = express()   //Creates an Express application. It is a top-level function exported by the express module.
 
-app.use((req, res, next)=>{
-    console.log(req.headers)
+app.use(morgan('dev'))
+
+app.use(express.static(__dirname + '/public')) // This will serve up files accordingly, this is enough
+
+app.use((req, res, next)=>{                    // This will be default behaviour if above implementation doesn't work due to errors
+    console.log(req.headers)                   //Not necessary
 
     res.setHeader('Content-Type', 'text/html')
     res.statusCode=200
